2009410c73f8fed36f6d3f344e4d60734791a665
Antonio <dangeloantonio179@gmail.com>
2024-02-26 18:20:25 +0100

    fix: effing dotenv

--- config/settings.php
+++ config/settings.php
@@ -1,7 +1,6 @@
 <?php
 
-//$dotenv = \Dotenv\Dotenv::create(__DIR__ . '/../');
-$dotenv = \Dotenv\Dotenv::createMutable(__DIR__ . '/../');
+$dotenv = \Dotenv\Dotenv::create(__DIR__ . '/../');
 $dotenv->load();
 // Should be set to 0 in production
 getenv('ENVIRONMENT') == 'prod' ? error_reporting(0) : error_reporting(E_ALL);
